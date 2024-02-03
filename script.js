124af93d7272b149981f81963c56d4fc4c6668f5
phamhuyen2702 <157961805+phamhuyen2702@users.noreply.github.com>
2024-02-03 18:13:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+function myFunction() {
+  alert('Hello World!');
+}
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Anh bíc thừa! 😘";
+  gif.src =
+    "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
+    
+    setTimeout(function() {
+      window.location.href = 'Huyen/index.html';
+      }, 4000); 
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+  
+  
+});
